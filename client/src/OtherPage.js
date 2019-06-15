dcc55bb097c593a69c369a805763716eff5eadeb
Ganesh Gaitonde <ganeshgaitonde@protonmail.com>
2019-06-16 02:07:54 +0530

    INitial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const OtherPage = () => {
+  return (
+    <div>
+      I'm other page!
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
+};
+
+export default OtherPage;
